324ad31231a7abc8cc14b8607b3cbc3677b5dbac
Muhari Márk <markmuhari.eu@gmail.com>
2021-08-23 07:03:23 +0200

    DestinationControllerIT flyway files done

--- CoderTravel/src/main/java/hu/progmasters/codertravel/dto/DestinationCreateCommand.java
+++ CoderTravel/src/main/java/hu/progmasters/codertravel/dto/DestinationCreateCommand.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -31,7 +30,7 @@ public class DestinationCreateCommand {
     @Schema(description = "location of id", example = "1")
     private Integer locationId;
 
-    @NotNull(message = "Location id must not be null!")
+    @NotNull(message = "Travel Agency id must not be null!")
     @Schema(description = "Travel Agency of id", example = "1")
     private Integer agencyId;
 }
--- CoderTravel/src/test/java/hu/progmasters/codertravel/controller/DestinationControllerIT.java
+++ CoderTravel/src/test/java/hu/progmasters/codertravel/controller/DestinationControllerIT.java
@@ -1,48 +1,140 @@
 package hu.progmasters.codertravel.controller;
 
+import hu.progmasters.codertravel.dto.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class DestinationControllerIT {
 
+    @Autowired
+    WebTestClient webTestClient;
+
+    private final String URI_PATH = "/api/destinations";
+    private final String URI_LOCATION_PATH = "/api/locations";
+    private final String URI_AGENCY_PATH = "/api/travelagencies";
+
+    private static LocationDestinationInfo locationDestinationInfo;
+    private static TravelAgencyDestinationInfo agencyDestinationInfo;
+    private static DestinationCreateCommand createCommand;
+    private static DestinationCreateCommand updateCommand;
+    private static DestinationCreateCommand createCommandNotValid;
+    private static DestinationInfo destinationInfo;
+    private static LocationCreateCommand createCommandLocation;
+    private static TravelAgencyCreateCommand createCommandAgency;
+
     @BeforeEach
     void setUp() {
-
+        ModelMapper modelMapper = new ModelMapper();
+        createData(modelMapper);
     }
 
     @Test
-    void test_FindAllDestinations() {
-
+    void test_FindAllDestinations_WithEmptyList() {
+        webTestClient.get().uri(URI_PATH)
+                .exchange().expectBody().json("[]");
     }
 
     @Test
     void test_FindDestinationById() {
-
+        test_CreateDestination();
+        webTestClient.get().uri(URI_PATH+"/1")
+                .exchange().expectStatus().isOk();
     }
 
     @Test
     void test_CreateDestination() {
+        webTestClient.post().uri(URI_LOCATION_PATH)
+                .bodyValue(createCommandLocation).exchange().expectStatus().isCreated();
 
+        webTestClient.post().uri(URI_AGENCY_PATH)
+                .bodyValue(createCommandAgency).exchange().expectStatus().isCreated();
+
+        webTestClient.post().uri(URI_PATH)
+                .bodyValue(createCommand).exchange().expectStatus().isCreated();
+
+    }
+
+    @Test
+    void test_CreateDestination_NotValidData() {
+        webTestClient.post().uri(URI_LOCATION_PATH)
+                .bodyValue(createCommandLocation).exchange().expectStatus().isCreated();
+
+        webTestClient.post().uri(URI_AGENCY_PATH)
+                .bodyValue(createCommandAgency).exchange().expectStatus().isCreated();
+
+        webTestClient.post().uri(URI_PATH)
+                .bodyValue(createCommandNotValid).exchange().expectStatus().isBadRequest();
     }
 
     @Test
     void test_ModifyDestination() {
+        test_CreateDestination();
 
+        webTestClient.put().uri(URI_PATH+"/1")
+                .bodyValue(updateCommand).exchange().expectStatus().isOk();
     }
 
     @Test
     void test_DeleteDestination() {
+        test_CreateDestination();
 
+        webTestClient.delete().uri(URI_PATH+"/1")
+                .exchange().expectStatus().isOk();
     }
 
+    private void createData(ModelMapper modelMapper) {
+        createCommand = new DestinationCreateCommand();
+        createCommand.setTitle("Debrecen de szép vagy!");
+        createCommand.setPricePerDay(4500);
+        createCommand.setDescription("Utazz Debrecenbe a virágkarneválkor!");
+        createCommand.setAgencyId(1);
+        createCommand.setLocationId(1);
 
+        updateCommand = new DestinationCreateCommand();
+        updateCommand.setTitle("Debrecen de szép vagy!");
+        updateCommand.setPricePerDay(3500);
+        updateCommand.setDescription("Utazz Debrecenbe a virágkarneválkor!");
+        updateCommand.setAgencyId(1);
+        updateCommand.setLocationId(1);
+
+        createCommandNotValid = new DestinationCreateCommand();
+        createCommandNotValid.setTitle(" ");
+        createCommandNotValid.setPricePerDay(-4500);
+        createCommandNotValid.setDescription("Utazz Debrecenbe a virágkarneválkor!");
+        createCommandNotValid.setAgencyId(1);
+        createCommandNotValid.setLocationId(1);
+
+        createCommandLocation = new LocationCreateCommand();
+        createCommandLocation.setIso("HUN");
+        createCommandLocation.setCountry("Hungary");
+        createCommandLocation.setCity("Budapest");
+        createCommandLocation.setStreetAndNumber("Lenkey u. 7.");
+
+        createCommandAgency = new TravelAgencyCreateCommand();
+        createCommandAgency.setLocationID(1);
+        createCommandAgency.setName("Travel Agency");
+
+//        locationDestinationInfo = new LocationDestinationInfo();
+//        locationDestinationInfo.setCity("Budapest");
+//        locationDestinationInfo.setCountry("Hungary");
+//        locationDestinationInfo.setStreetAndNumber("Lenkey utca 7.");
+//
+//
+//        agencyDestinationInfo = new TravelAgencyDestinationInfo();
+//        agencyDestinationInfo.setId(1);
+//        agencyDestinationInfo.setName("Otp Travel");
+
+
+    }
 }
